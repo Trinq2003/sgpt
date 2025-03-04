1ede0aae59cc414471dda6a66c9743196fe14617
Trinq2003 <tri.nq210860@sis.hust.edu.vn>
2025-03-04 16:55:00 +0700

    Turn off all safe_serialization

--- train.py
+++ train.py
@@ -144,8 +144,8 @@ def train(args, train_dataset, eval_dataset, model: PreTrainedModel, tokenizer: 
 
             logger.info("Saving model checkpoint to %s", output_dir)
             global_eval=results["loss"]
-            model_to_save.save_pretrained(output_dir)
-            tokenizer.save_pretrained(output_dir)
+            model_to_save.save_pretrained(output_dir, safe_serialization=False)
+            tokenizer.save_pretrained(output_dir, safe_serialization=False)
 
             torch.save(args, os.path.join(output_dir, "training_args.bin"))
             with open(os.path.join(output_dir, "params.json"), "w") as jsonfile:
@@ -337,7 +337,7 @@ def main():
                 model.module if hasattr(model, "module") else model
             )  # Take care of distributed/parallel training
             model_to_save.save_pretrained(args.output_dir, safe_serialization=False)
-            tokenizer.save_pretrained(args.output_dir)
+            tokenizer.save_pretrained(args.output_dir, safe_serialization=False)
 
             # Good practice: save your training arguments together with the trained model
             torch.save(args, os.path.join(args.output_dir, "training_args.bin"))
